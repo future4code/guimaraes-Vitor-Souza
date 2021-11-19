fb3c2dc54eb2aaec0e39db6bc40b5641d294f266
Vitor Vasconcelos <vitorvsouzaa@gmail.com>
2021-11-18 21:11:29 -0300

    Anexei os exercicios da aula de variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,56 @@
+/* Exercicio de enterpretação 
+
+EXERC. 1
+
+// Imprime na tela o valor da variavel, resultando em 10.
+
+let a = 10
+let b = 10
+
+console.log(b)
+
+// Imprime e concatena os valores, resultando em 15.
+b = 5
+console.log(a, b)
+
+
+
+EXERC. 2
+
+//Nao consegui compreender.
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+*/
+
+
+
+/*Exercícios de escrita de código
+
+EXERC. 1 
+/*
+let nome
+let idade
+
+console.log( typeof nome, idade)
+// Resultado fica indefined porque não há valor para variável.
+*/
+/*
+const nome=prompt("Qual seu nome?") 
+const idade=prompt("qual sua idade?")
+
+console.log( typeof nome, idade)
+// Resultado volta string por ter recebido valor e concatena com numeral da idade.
+
+console.log("Olá ", nome, ",você tem", idade, "anos")
+console.log( typeof nome, idade)
+*/
+
+
+
